20ca3dce824bb5d9ed966c33664f4d251994c03a
NikoHeikki <niko.a.heikkila@gmail.com>
2017-10-26 18:03:44 +0300

    päivitys

--- src/main/java/com/jaheikki/App.java
+++ src/main/java/com/jaheikki/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hei maailma!!!!!!!!!!!!!");
+        System.out.println( "hello niko" );
+        System.out.println( "hello jari" );
+        System.out.println( "hello jari!!!" );
+
+
     }
 }
